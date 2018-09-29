007e3c33db1076ea90a52ff970d31c45da72bf4b
Ilya Naydanov <turbcool@gmail.com>
2018-09-29 20:53:27 +0500

    v0.3 Document number validation

--- app/components/doc-form.js
+++ app/components/doc-form.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    actions: {
+        sendNumber(doc_number) {
+            //AAAAAA000-AA
+            if (doc_number.match('[A-Z]{6}[0-9]{3}-[A-Z]{2}')) 
+                this.set('val_result','');
+            else this.set('val_result','Номер документа введён неверно');
+        }
+    }
 });
--- app/routes/course.js
+++ app/routes/course.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    
     model() {
         return ['Теоретические основы информатики',
         'Дискретная математика',
