c642177b923fc9182d5624454d24285a5b613b7c
Ilya Naydanov <turbcool@gmail.com>
2018-10-03 01:47:07 +0500

    v0.5.3

--- app/controllers/document.js
+++ app/controllers/document.js
@@ -3,6 +3,8 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   docNumber: null,
   doc_unit_result: null,
+  error_text: null,
+  show_all:false,
   actions: {
     generateRecords(){
       this.store.unloadAll();
@@ -41,16 +43,29 @@ export default Controller.extend({
     getDocument(number){
       var peekAll = this.store.peekAll('document');
 
+      //Search for id:
       var array = peekAll.toArray();
-      var id=0;
+      var id=-1;
       array.forEach(function(item, i, array){
         if (item.numb==number)
           id=i;
       });
 
-      var peekId = this.store.peekRecord('document', id);
+      //Peek id если он был найден:
+      if (id==-1){
+        this.set('doc_unit_result', null);
+        this.set('error_text', "Такого документа не существует");
+      }
+      else
+      {
+        this.set('error_text', "");
+        var peekId = this.store.peekRecord('document', id);
+        this.set('doc_unit_result', peekId);
+      }
+    },
 
-      this.set('doc_unit_result', peekId);
+    showAll(){
+      this.set("show_all",true);
     }
   }
 });
