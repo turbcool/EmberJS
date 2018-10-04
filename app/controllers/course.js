0bde961c9a656f55e561db3694558ff3a17fc445
Ilya Naydanov <turbcool@gmail.com>
2018-10-04 22:42:07 +0500

    v0.7.2 50%

--- app/controllers/course.js
+++ app/controllers/course.js
@@ -2,11 +2,39 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   course_list:[],
+  test_array:[],
+  S1_P1:[],
+  S1_P2:[],
+  S1_P3:[],
+  S2_P1:[],
+  S2_P2:[],
+  S2_P3:[],
   actions:{
     addToList(dragged, target){
+    },
+    S1_P1(dragged,target){
       this.get('model').removeObject(dragged);
-      this.get('course_list').pushObject(dragged);
-      //target.target.set('test_array', [{name: "hello"}]);
+      this.get('S1_P1').pushObject(dragged);
+    },
+    S1_P2(dragged,target){
+      this.get('model').removeObject(dragged);
+      this.get('S1_P2').pushObject(dragged);
+    },
+    S1_P3(dragged,target){
+      this.get('model').removeObject(dragged);
+      this.get('S1_P3').pushObject(dragged);
+    },
+    S2_P1(dragged,target){
+      this.get('model').removeObject(dragged);
+      this.get('S2_P1').pushObject(dragged);
+    },
+    S2_P2(dragged,target){
+      this.get('model').removeObject(dragged);
+      this.get('S2_P2').pushObject(dragged);
+    },
+    S2_P3(dragged,target){
+      this.get('model').removeObject(dragged);
+      this.get('S2_P3').pushObject(dragged);
     }
   }
 });
